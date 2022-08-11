e49416ee17d4419768beab7638634bfa7c6cb7cb
DmitriyKalyanov <lightbearer1337@gmail.com>
2022-08-11 22:34:36 +0500

    добавил кнопку удалить

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Counter from "./counter";
 
 const CountersList = () => {
-    return <h1>Counters List</h1>
+    const [counters, setCounters] = useState([
+        { id: 0, value: 0, name: 'Ненужная вещь' }, 
+        { id: 1, value: 4, name: 'Ложка'  }, 
+        { id: 2, value: 0, name: 'Вилка'  },
+        { id: 3, value: 0, name: 'Тарелка'  },
+        { id: 4, value: 0, name: 'Набор минималиста'  },
+    ])
+    const handleDelete = () => {
+        console.log('handleDelete');
+    }
+    return (
+        <>
+            {counters.map((count) => (
+                <Counter key={count.id} value={count.value} name={count.name} onDelete={handleDelete}/>
+            ))}
+        </>
+    )
 }
 
 export default CountersList
